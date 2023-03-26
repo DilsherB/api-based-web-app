661f36b6ea578bfee34ff9300f41366ad30186de
Dilsher <dilsherb@gmail.com>
2023-03-26 16:01:55 +0300

    get data function updated.

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,11 +1,11 @@
 const getData = async (url) => {
   const basicData = await fetch(url);
-  const jsonFormat = basicData.json();
+  const jsonFormat = await basicData.json();
   return jsonFormat;
 }
 
-const mainAPI = getData('');
+const mainAPI = getData('https://api.tvmaze.com/search/shows?q=girls');
 const secAPI = getData('');
 const other = getData('');
-
+console.log(mainAPI)
 export { mainAPI, secAPI, other };
